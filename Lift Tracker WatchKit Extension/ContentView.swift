24c8614745a660c2d4a6df914270279efeec98a6
Tyler Webner <tyler.webner@appfolio.com>
2021-03-18 15:42:05 -0700

    Create watch app that records accelerometer data

--- Lift Tracker WatchKit Extension/ContentView.swift
+++ Lift Tracker WatchKit Extension/ContentView.swift
@@ -6,11 +6,50 @@
 //
 
 import SwiftUI
+import CoreMotion
 
 struct ContentView: View {
+    @State private var isRecording = false
+    @State private var accelerationData: [CMAcceleration] = []
+    //    @State private var recordingTime
+    let motion = CMMotionManager()
+
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        let buttonText = isRecording ? "Stop" : "Start"
+        let buttonAction = isRecording ? stopRecording : startRecording
+        Button(buttonText, action: buttonAction)
+        if isRecording {
+            
+        }
+    }
+    
+    func startRecording() {
+        accelerationData.removeAll()
+        isRecording = true
+        if motion.isDeviceMotionAvailable {
+            motion.deviceMotionUpdateInterval = 1.0 / 1.0
+            motion.showsDeviceMovementDisplay = true
+            motion.startDeviceMotionUpdates(to: OperationQueue.current!,
+                                            withHandler: { (data, error) in
+                                                if let validData = data {
+                                                    accelerationData.append(validData.userAcceleration)
+                                                }
+                                            })
+        }
+    }
+        
+    func stopRecording() {
+        isRecording = false
+        motion.stopDeviceMotionUpdates()
+        
+        print("Acceleration Data:")
+        accelerationData.forEach { accelerationReading in
+            print("x: \(truncate(number: accelerationReading.x))\ty: \(truncate(number: accelerationReading.y))\tz: \(truncate(number: accelerationReading.z))")
+        }
+    }
+    
+    private func truncate(number: Double) -> String {
+        return String(format: "%.3f", number)
     }
 }
 
